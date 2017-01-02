232d71892d5e9774134d7f43017009b3ac7102ef
le-garff-yoann <pe.weeble@yahoo.fr>
2017-01-02 17:52:17 +0100

    update copyright interval: 2015-2016 -> 2015-2017

--- lib/Navel/Event.pm
+++ lib/Navel/Event.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+# Copyright (C) 2015-2017 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 # navel-event is licensed under the Apache License, Version 2.0
 
 #-> BEGIN
@@ -89,7 +89,7 @@ Navel::Event
 
 =head1 COPYRIGHT
 
-Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+Copyright (C) 2015-2017 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 
 =head1 LICENSE
 
